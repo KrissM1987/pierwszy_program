415c91b1d21f53ba3ab3adf126d5015e076d47b4
KrissM1987 <krissm@poczta.fm>
2018-10-11 16:52:47 +0200

    proba2

--- piewszy_program.c
+++ piewszy_program.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-
+#include <math.h>
 void main(void)
 {
-	puts("Kot ma Ale");
-	
+	puts("Kot ma Ale \n a Ala kajs pojechala");
+	printf("LALALA\n");
+	printf("czemu sie to nie aktualizuje");
+	int  i;
+	for (i=1; i<=4 ; ++i)
+{
+	printf("%d\n ", i*i);
+
+}
 
 }
